ade7182ecebaf8231ad85aaf6a2b2be2662c16f3
mariandrea <pericoandrea@gmail.com>
2022-11-23 03:18:46 -0500

    read smart contract

--- src/App.js
+++ src/App.js
@@ -4,6 +4,42 @@ import { ethers } from "ethers";
 
 function App() {
   const [address, setAddress] = useState("");
+  const [provider, setProvider] = useState();
+  const [name, setName] = useState("");
+
+  async function set() {
+    const contractAddress = "0x0DC7c9547756602218f96AB820c49E30cB3b97eB";
+    const contractAbi = [
+      {
+        inputs: [],
+        name: "name",
+        outputs: [
+          {
+            internalType: "string",
+            name: "",
+            type: "string",
+          },
+        ],
+        stateMutability: "view",
+        type: "function",
+      },
+    ];
+
+    if (provider) {
+      // The Contract object
+      const daiContract = new ethers.Contract(
+        contractAddress,
+        contractAbi,
+        provider
+      );
+      console.log(daiContract);
+      const valueName = await daiContract.name();
+      setName(valueName);
+      console.log(valueName);
+    } else {
+      alert("Conectese a MetaMask");
+    }
+  }
 
   return (
     <div className="App">
@@ -14,19 +50,21 @@ function App() {
           if (window.ethereum) {
             // A Web3Provider wraps a standard Web3 provider, which is
             // what MetaMask injects as window.ethereum into each page
-            const provider = new ethers.providers.Web3Provider(window.ethereum);
+            const prov = new ethers.providers.Web3Provider(window.ethereum);
+            setProvider(prov);
 
             // MetaMask requires requesting permission to connect users accounts
-            await provider.send("eth_requestAccounts", []);
-            console.log(provider);
-            
+            await prov.send("eth_requestAccounts", []);
+            console.log(prov);
+
             // The MetaMask plugin also allows signing transactions to
             // send ether and pay to change state within the blockchain.
             // For this, you need the account signer...
-            const signer = provider.getSigner();
+            const signer = prov.getSigner();
 
-            const addr = await signer.getAddress();
-            setAddress(addr);
+            setAddress(await signer.getAddress());
+
+            set();
           } else {
             alert("¡Obtenga la extensión de MetaMask para su navegador!");
           }
@@ -34,7 +72,8 @@ function App() {
       >
         MetaMask
       </button>
-      {address === ""? "" : <h2>Dirección de la billetera: {address} </h2>}
+      {address === "" ? "" : <h2>Billetera: {address} </h2>}
+      {name === "" ? "" : <h2>Nombre Contrato: {name} </h2>}
     </div>
   );
 }
