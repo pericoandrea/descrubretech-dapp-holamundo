1f6409a1bf4caa6bb7cc4086973d82ae8ed7b7e7
mariandrea <pericoandrea@gmail.com>
2022-11-23 01:27:29 -0500

    connect wallet with ethers.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { ethers } from "ethers";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Conectar con la billetera</h1>
+      <button
+        onClick={async () => {
+          alert("Conectar a MetaMask!!");
+          if (window.ethereum) {
+            // A Web3Provider wraps a standard Web3 provider, which is
+            // what MetaMask injects as window.ethereum into each page
+            const provider = new ethers.providers.Web3Provider(window.ethereum);
+
+            // MetaMask requires requesting permission to connect users accounts
+            await provider.send("eth_requestAccounts", []);
+
+            // The MetaMask plugin also allows signing transactions to
+            // send ether and pay to change state within the blockchain.
+            // For this, you need the account signer...
+            const signer = provider.getSigner();
+            console.log(provider, await signer.getAddress());
+          }
+        }}
+      >
+        MetaMask
+      </button>
     </div>
   );
 }
